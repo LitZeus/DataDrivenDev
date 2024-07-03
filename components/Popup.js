a5813fea5511ccc0c9f5eaacf52c46baa33f018e
tejasathalye <me.tejasathalye@gmail.com>
2024-07-03 17:53:39 +0530

    Update pop-up visuals

--- components/Popup.js
+++ components/Popup.js
@@ -5,14 +5,18 @@ const Popup = ({ onClose }) => {
   return (
     <div className={styles.popupOverlay}>
       <div className={styles.popup}>
-        <h3>Download Resume</h3>
-        <a href="/resume.docx" download target="_blank" rel="noopener noreferrer">
-          <button className={styles.popupButton}>Download as DOCX</button>
-        </a>
-        <a href="/resume.pdf" download target="_blank" rel="noopener noreferrer">
-          <button className={styles.popupButton}>Download as PDF</button>
-        </a>
-        <button className={styles.popupClose} onClick={onClose}>Close</button>
+        <button className={styles.popupClose} onClick={onClose}>
+        ×
+        </button>
+        <h2 className={styles.popupHeading}>Download Resume</h2>
+        <div className={styles.buttonContainer}>
+          <a href="/resume.docx" download target="_blank" rel="noopener noreferrer">
+            <button className={styles.popupButton}>Download as DOCX</button>
+          </a>
+          <a href="/resume.pdf" download target="_blank" rel="noopener noreferrer">
+            <button className={styles.popupButton}>Download as PDF</button>
+          </a>
+        </div>
       </div>
     </div>
   );
