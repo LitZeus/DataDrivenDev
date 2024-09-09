67054b33bcb4827cb7072b9ea37650a95aa7c0b7
tejasathalye <me.tejasathalye@gmail.com>
2024-09-09 09:21:46 +0530

    Updated Details

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,9 +5,11 @@ const AboutPage = () => {
     <>
       <h2>A Little Bit About Me</h2>
       <br /><br />
-      <p>Highly motivated Computer Engineering student with a passion for data science, machine learning, and cloud computing. Skilled in programming, data analysis, and visualization, with strong problem-solving and critical thinking abilities. Seeking to leverage my expertise in developing innovative solutions, and my effective communication and leadership skills, to contribute to a dynamic team in a challenging and growth-oriented environment.</p>
+      <p> Driven Computer Engineering student seeking an entry-level AI/ML role. Proficient in Python, Java, C++, and cloud technologies. Eager to apply technical skills to real-world projects, expand knowledge through advanced studies, and collaborate with experienced professionals. Passionate about solving complex problems and contributing to innovative AI/ML solutions.</p>
       <br />
-      <p>I have a deep passion for travel photography, using my journeys as inspiration to create compelling visual content. Alongside photography, I find joy in expressing creativity through pencil sketches, infusing detail and imagination into my artwork. One of my biggest aspirations is to climb Mt. K2, fueled by a sense of adventure and a determination to conquer challenging goals. These interests reflect my adventurous spirit, creative mindset, and dedication to personal growth and achievement.</p>
+      <p>I believe in: "The more you know, the more you realize how much less you know." This fosters my learning curve and motivates me to develop my knowledge and skills, and be a better version of myself through continuous learning.</p>
+      <br />
+      <p>Besides, I love to do pencil sketching. This also teaches me to grow continuously and contribute to the art and sketchers community. Alongside, I have also a deep passion for travel photography and trekking. One of my biggest aspirations is to climb Mt. K2 with the determination to conquer challenging goals</p>
       <br />
       <p>Beyond my professional pursuits, I am an avid sports enthusiast and a three-time Maharashtra state Gold medalist in Karate. Competing in the National Games thrice has taught me perseverance, focus, and discipline—qualities I bring to all areas of my life and work.</p>
       <br /><br />
